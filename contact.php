bb117108bf204609dc597a38238b54b720305dc9
Magnus Bruskerud <magnusvrb@gmail.com>
2014-03-13 20:56:25 +0100

    noe

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html> 
+  <head>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>Contact us</title>
+  </head>
+  <body>
+
+	<div id="header">
+      <p>Contact us</p>
+	</div>
+
+	<div id="leftmenybox">  
+      <div id="menybar">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <p>Homepage</p>
+          <li><a href="about.php">About</a></li>
+          <p>Here is our history</p>
+          <li><a href="games.php">Games</a></li>
+          <p>Hope you enjoy our games!</p>
+          <li><a href="contact.php">Contact</a></li> 
+          <p>Question? Send us a mail!</p>
+        </ul>
+      </div>
+    </div>
+	<div id="rightmenybox"></div> 
+
+	<footer class="mainfooter">
+		<p>Copyright 2014 &copy; <a href="#" title="prothero.kd.io">Prothero-Mcclane654</a></p>
+	</footer>
+    
+  </body>
+</html>
